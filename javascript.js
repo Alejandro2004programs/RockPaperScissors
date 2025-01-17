0a34d4d9935fc8d820c355d1454d6c0ac16946fb
Alejandro Alvarez <ala007@ucsd.edu>
2025-01-16 22:02:01 -0800

    finished the rock paper scissors game played on the console

--- javascript.js
+++ javascript.js
@@ -1,9 +1,7 @@
-console.log("Testing to see that the javascript file is working fine")
-
 function getComputerChoice() {
     randomNumber = Math.random();
     if(randomNumber < .333) {
-        return "scissors"
+        return "scissors";
     }
     else if (randomNumber < .666) {
         return "rock";
@@ -14,10 +12,43 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    let choice = prompt("Welcome to rock, paper, scissors. Pick one of the following to play against the computer: 1) rock 2) paper 3) scissors", null);
+    let choice = prompt(`Welcome to a new game. Pick between rock, paper, scissors. The current score is: You: ${humanScore}, Computer: ${computerScore}`, null);
+    choice = choice.toLowerCase();
     return choice;
 }
 
+function playRound() {
+    let computerChoice = getComputerChoice();
+    let humanChoice = (getHumanChoice()).toLowerCase();
 
-let humanChoice = getHumanChoice();
-console.log(humanChoice);
+    // I could probably write this cleaner using a nested switch statement. I will look into that later.
+    if(humanChoice == computerChoice) {
+        console.log(`You tied. The computer choice was ${computerChoice}. Your choice was ${humanChoice}`);
+    }
+    else if(humanChoice == "scissors" && computerChoice == "paper" || humanChoice == "rock" && computerChoice == "scissors" || humanChoice == "paper" && computerChoice == "rock") {
+        console.log(`You won. The computer choice was ${computerChoice}. Your choice was ${humanChoice}`);
+        humanScore += 1;
+    }
+    else {
+        console.log(`You lost. The computer choice was ${computerChoice}. Your choice was ${humanChoice}`);
+        computerScore += 1;
+    }
+
+    return;
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playGame() {
+    let i = 0;
+    while (i < 5) {
+        playRound();
+        console.log(`Your score is ${humanScore}. The computer score is ${computerScore}`);
+        i += 1;
+    }
+    return;
+}
+
+playGame();
+
