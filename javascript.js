74ef67f87228f1c176e3bb6e250ce97afa43bbc6
Alejandro Alvarez <ala007@ucsd.edu>
2025-01-16 20:59:40 -0800

    getComputerChoice function works well

--- javascript.js
+++ javascript.js
@@ -1,1 +1,15 @@
 console.log("Testing to see that the javascript file is working fine")
+
+function getComputerChoice() {
+    randomNumber = Math.random();
+    if(randomNumber < .333) {
+        return "scissors"
+    }
+    else if (randomNumber < .666) {
+        return "rock";
+    }
+    else {
+        return "paper";
+    }
+}
+
