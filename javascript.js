00ffdc41a50e33c31cbe895f00ba755ca2af611a
Alejandro Alvarez <ala007@ucsd.edu>
2025-01-16 21:08:30 -0800

    getHumanChoice function is working well

--- javascript.js
+++ javascript.js
@@ -13,3 +13,11 @@ function getComputerChoice() {
     }
 }
 
+function getHumanChoice() {
+    let choice = prompt("Welcome to rock, paper, scissors. Pick one of the following to play against the computer: 1) rock 2) paper 3) scissors", null);
+    return choice;
+}
+
+
+let humanChoice = getHumanChoice();
+console.log(humanChoice);
